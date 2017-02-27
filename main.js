29b821127c2b0407782543044593d7f283d5b5e4
arihito <arihito.m@gmail.com>
2017-02-27 21:45:52 +0900

    props add

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 var HelloWorld = React.createClass({
   render: function() {
-    return React.createElement('h2', null, 'Hello, React World');
+    return React.createElement('h2', null, 'Hello, ' + this.props.name + ' World');
   }
 });
 
 ReactDOM.render(
-  React.createElement(HelloWorld),
+  React.createElement(
+    HelloWorld,
+    {
+      name: 'React'
+    }
+  ),
   document.getElementById('content')
 );
