306b15dc9966678fe46749124c6f30822e92a377
arihito <arihito.m@gmail.com>
2017-02-27 23:05:14 +0900

    state in evalfunction

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-var HelloWorld = React.createClass({
+var MathClass = React.createClass({
+  getInitialState: function() {
+    return {
+      num: ''
+    };
+  },
+  mathFunc: function(e) {
+    if (e.key === 'Enter')
+      this.setState({
+        num: e.target.value
+    });
+  },
   render: function() {
-    return React.DOM.div(null, 
-      React.DOM.h2(null, 'First Element'),
-      React.DOM.h2(null, 'Second Element'),
-      React.DOM.h2(null, 'Third Element')
+    return React.DOM.div(
+      null,
+      React.DOM.input({
+        type: 'test',
+        onKeyPress: this.mathFunc
+      }),
+      React.DOM.h2(null,eval(this.state.num))
     );
   }
 });
 
 ReactDOM.render(
   React.createElement(
-    HelloWorld,
-    {
-      name: 'React'
-    }
+    MathClass
   ),
   document.getElementById('content')
 );
